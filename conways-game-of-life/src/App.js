04a0fa06a23da8e9350de6e54ea8fa7cabf53bd8
James Wigdor <jpwigdor@gmail.com>
2020-06-25 11:49:28 -0400

    create clickable grid

--- conways-game-of-life/src/App.js
+++ conways-game-of-life/src/App.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import produce from 'immer';
 import './App.css';
 
 const numRows = 50;
@@ -25,6 +26,12 @@ function App() {
         rows.map((col, k) => 
           <div 
           key={`${i}-${k}`}
+          onClick={() => {
+            const newGrid = produce(grid, gridCopy => {
+              gridCopy[i][k] = grid[i][k] ? 0 : 1;
+            })
+            setGrid(newGrid)
+          }}
             style={{width: 20, height: 20, 
             backgroundColor: grid[i][k] ? 'pink' : undefined,
             border: 'solid 1px black'
